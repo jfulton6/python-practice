ef5d0fc747f7410648555ab0f55093df7d6e1c2c
jfulton6 <jfulton6@nait.ca>
2023-04-26 17:10:08 -0600

    basics p1

---
+++ python-basics/input.py
@@ -0,0 +1,5 @@
+#to get input you simpaly use the term input()
+
+name = input("What is your Name? ") #stores name
+
+print("Hello,", name) #prints out name
---
+++ python-basics/variables.py
@@ -0,0 +1,14 @@
+#lables on your objects are called variables
+#these are object references
+
+print("Hello Jacob")
+#unlike c# you don't have to name your variables string, double, decimal, int it does that automatically like var, so you have to be careful
+
+first_name = "Ada"
+print(first_name)
+#to seperate points you can use a comma
+print("these are", "joined")
+#string interpolation
+print("Hello", first_name)
+
+#if we were to change first_name = 11 it would change it from ada to 11 without having to specify the int.
